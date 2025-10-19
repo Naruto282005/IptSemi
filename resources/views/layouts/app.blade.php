2b0f01edbeb117c6c2b6430082005f83ba0ee5d7
Jsabs <sabillano057@gmail.com>
2025-10-20 00:47:44 +0800

    FooterStick

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,31 @@
     <meta charset="UTF-8">
     <title>Gym Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        /* Reset default margins and make layout fill the screen */
+        html, body {
+            height: 100%;
+            margin: 0;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        main {
+            flex: 1;
+        }
+
+        footer {
+            background-color: #212529;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+            margin-top: auto;
+        }
+    </style>
 </head>
 <body>
     @include('partials.navbar')
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="bg-dark text-white text-center py-3 mt-4">
+<footer>
     <small>&copy; {{ date('Y') }} Gym Management System</small>
 </footer>
